81ab478b31bfe2edb1d8af892d5df55e7d2077f2
William Tomé <william.tomenh@gmail.com>
2021-01-26 11:40:16 -0300

    Adicionado a fonte no componente head do next.

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
 import db from '../db.json'
+import Head from 'next/head'
 
 const GlobalStyle = createGlobalStyle`
   * {
@@ -30,6 +31,10 @@ const theme = db.theme
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet" />
+      </Head>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <Component {...pageProps} />
