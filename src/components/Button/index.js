a3e94e2aa03800c83d875261dbb9b0babd5fb1b1
William Tomé <william.tomenh@gmail.com>
2021-01-26 20:00:45 -0300

    Adicionado os estilos do componente input.

---
+++ src/components/Button/index.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  width: 100%;
+  height: 40px;
+  background-color: #E91E63;
+  border-radius: 4px;
+  box-shadow: 0px 0px 2px rgba(0, 0, 0, 0.12), 0px 2px 2px rgba(0, 0, 0, 0.24);
+  text-transform: uppercase;
+  text-align: center:
+  letter-spacing: 1.25px;
+  font-size: 14px;
+  line-height: 16px;
+  font-weight: bold;
+  color: #FFFFFF;
+  margin-top: 30.5px;
+`;
+
+export default Button;
