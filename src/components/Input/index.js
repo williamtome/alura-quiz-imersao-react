f73c9b24225578a0ed40cc9d79964dfaae13fe40
William Tomé <william.tomenh@gmail.com>
2021-01-26 20:01:00 -0300

    Adicionado os estilos do componente input.

---
+++ src/components/Input/index.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const Input = styled.input`
+  background-color: #171B35;
+  border: 1px solid #6200EE;
+  border-radius: 3.5px;
+  color: #FFFFFF;
+  font-size: 14px;
+  letter-spacing: 0.15px;
+  line-height: 24px;
+  width: 100%;
+  height: 40px;
+  margin-top: 15px;
+`;
+
+export default Input;
